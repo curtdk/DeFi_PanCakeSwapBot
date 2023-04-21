64a52e606ab5002d6f2e2beae2c443c0da30a23f
curtdk <942233@qq.com>
2023-04-21 17:49:37 +0800

    11

--- config.py
+++ config.py
@@ -1,10 +1,18 @@
 import os
 
-YOUR_WALLET_ADDRESS = os.environ.get('WALLET_ADDRESS')  # Add Your Wallet Address here by removing whole line
-YOUR_PRIVATE_KEY = os.environ.get('PRIVATE_KEY')  # Add Your Private Key here by removing whole line
 
-TRADE_TOKEN_ADDRESS = None  # Add token address here example : "0xc66c8b40e9712708d0b4f27c9775dc934b65f0d9"
-WBNB_ADDRESS = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
+YOUR_WALLET_ADDRESS = '0x503B72d049728a85A5b3850AA106D7Ed8f289685'  # Add Your Wallet Address here by removing whole line
+YOUR_PRIVATE_KEY = '75b9cf03f85c602e80ca118a6a3cdbe51a3c17796fe0ca8ec613a83dcf22bd49'  # Add Your Private Key here by removing whole line
+
+TRADE_TOKEN_ADDRESS = "0x768a62a22b187EB350637e720ebC552D905c0331"  # Add token address here example : "0xc66c8b40e9712708d0b4f27c9775dc934b65f0d9"
+# WBNB_ADDRESS = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
+WBNB_ADDRESS = "0x55d398326f99059fF775485246999027B3197955"
+
+# YOUR_WALLET_ADDRESS = os.environ.get('WALLET_ADDRESS')  # Add Your Wallet Address here by removing whole line
+# YOUR_PRIVATE_KEY = os.environ.get('PRIVATE_KEY')  # Add Your Private Key here by removing whole line
+
+# TRADE_TOKEN_ADDRESS = None  # Add token address here example : "0xc66c8b40e9712708d0b4f27c9775dc934b65f0d9"
+# WBNB_ADDRESS = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
 PANCAKE_ROUTER_ADDRESS = "0x10ED43C718714eb63d5aA57B78B54704E256024E"
 SHOW_TX_ON_BROWSER = True
 
